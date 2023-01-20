72a614e648a65d2b5c23fbab54c8e69221e93fc9
joserasilla-uneat <jose.rasilla@alumnos.uneatlantico.es>
2023-01-20 09:06:19 +0100

    Update Pacman.java

--- Pacman.java
+++ Pacman.java
@@ -1,11 +1,16 @@
 import java.util.Scanner;
 
 public class Pacman {
-    public static void main(String[] args) {
 
-        Scanner entrada = new Scanner(System.in);
-        char inputUsuario;
-        boolean terminar = false;
+    static final String DIBUJO_JUGADOR = " P ";
+    static final String DIBUJO_FANTASMA = " F ";
+    static final String DIBUJO_PARED = "[ ]";
+    static final String DIBUJO_PASTILLA = " . ";
+    static final String DIBUJO_AIRE = "   ";
+    
+    static boolean terminar = false;
+
+    public static void main(String[] args) {
 
         int[][] unaMatriz = {
                 { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
@@ -26,40 +31,60 @@ public class Pacman {
         int[] posicionFantasma = { 5, 10 };
 
         do {
-            for (int laFila = 0; laFila < unaMatriz.length; laFila++) {
-                for (int laColumna = 0; laColumna < unaMatriz[laFila].length; laColumna++) {
-                    if (laFila == posicionPersonaje[0] && laColumna == posicionPersonaje[1]) {
-                        System.out.print("P");
-                    } else if (laFila == posicionFantasma[0] && laColumna == posicionFantasma[1]) {
-                        System.out.print("F");
-                    } else {
-                        if (unaMatriz[laFila][laColumna] == 0) {
-                            System.out.print(" ");
-                        } else if (unaMatriz[laFila][laColumna] == 1) {
-                            System.out.print("#");
-                        }
-                    }
-                }
-                System.out.println();
-            }
-
-            inputUsuario = entrada.nextLine().charAt(0);
-            switch (inputUsuario) {
-                case 's', 'S', '8':
-                    posicionPersonaje[0] = posicionPersonaje[0] + 1;
-                    break;
-                case 'w', 'W', '2':
-                    posicionPersonaje[0] = posicionPersonaje[0] - 1;
-                    break;
-                case 'a', 'A', '4':
-                    posicionPersonaje[1] = posicionPersonaje[1] - 1;
-                    break;
-                case 'd', 'D', '6':
-                    posicionPersonaje[1] = posicionPersonaje[1] + 1;
-                    break;
-                case 'f', 'F':
-                    terminar = true;
-            }
+            dibujarJuego(unaMatriz, posicionPersonaje, posicionFantasma);
+            moverJugador(posicionPersonaje);
         } while (!terminar);
+    }
+
+    static void dibujarJuego(int[][] mapa, int[] posicionJugador, int[] posicionFantasma) {
+        for (int laFila = 0; laFila < mapa.length; laFila++) {
+            for (int laColumna = 0; laColumna < mapa[laFila].length; laColumna++) {
+                if (laFila == posicionJugador[0] && laColumna == posicionJugador[1]) {
+                    System.out.print(DIBUJO_JUGADOR);
+                } else if (laFila == posicionFantasma[0] && laColumna == posicionFantasma[1]) {
+                    System.out.print(DIBUJO_FANTASMA);
+                } else {
+                    dibujarMapa(mapa[laFila][laColumna]);
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    static void dibujarMapa(int casillaMapa) {
+        switch (casillaMapa) {
+            case 0:
+                System.out.print(DIBUJO_PASTILLA);
+                break;
+            case 1:
+                System.out.print(DIBUJO_PARED);
+                break;
+            case 2:
+                System.out.print(DIBUJO_AIRE);
+                break;
+        }
+    }
+
+    static void moverJugador(int[] posicionPersonaje) {
+        Scanner entrada = new Scanner(System.in);
+        char inputUsuario;
+        inputUsuario = entrada.nextLine().charAt(0);
+        switch (inputUsuario) {
+            case 's', 'S', '8':
+                posicionPersonaje[0] = posicionPersonaje[0] + 1;
+                break;
+            case 'w', 'W', '2':
+                posicionPersonaje[0] = posicionPersonaje[0] - 1;
+                break;
+            case 'a', 'A', '4':
+                posicionPersonaje[1] = posicionPersonaje[1] - 1;
+                break;
+            case 'd', 'D', '6':
+                posicionPersonaje[1] = posicionPersonaje[1] + 1;
+                break;
+            case 'f', 'F':
+                terminar = true;
+        }
+        entrada.close();
     }
 }
