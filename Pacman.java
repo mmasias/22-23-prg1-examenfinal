1ae720d08b833200fd317acc1178410617d704ba
Pablo <pablo.arce1@alumnos.uneatlantico.es>
2023-01-20 09:51:55 +0100

    1 hora

--- Pacman.java
+++ Pacman.java
@@ -7,30 +7,39 @@ public class Pacman {
         int[][] terreno = {
                 { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
                 { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-                { 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1 },
+                { 1, 3, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 3, 1 },
                 { 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1 },
                 { 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1 },
                 { 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0 },
                 { 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1 },
                 { 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1 },
                 { 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1 },
-                { 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1 },
+                { 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1 },
                 { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
                 { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
         };
-
         int[] posPersonaje = { 7, 10 };
         int[] posFantasma = { 5, 10 };
         int[] contadorPuntos = {0};
+        boolean invencibilidad = false;
+        int [] skinActual = {1};
         while(terminar == false){
-            mostrarTerreno(terreno, posPersonaje, posFantasma, contadorPuntos);
-            terminar = moverse(posPersonaje, posFantasma);
+            skins(skinActual, terreno, posPersonaje, posFantasma, contadorPuntos);
+            terminar = moverse(posPersonaje, posFantasma, skinActual);
+            invencibilidad = bolaInvencibilidad(terreno, posPersonaje, contadorPuntos); 
+            terminar = interaccionFantasma(posFantasma, posPersonaje, contadorPuntos, invencibilidad);
             comerBolitas(terreno, posPersonaje, contadorPuntos);
         }
     }
 
-    static void mostrarTerreno(int [][] terreno, int [] posPersonaje, int [] posFantasma, int [] contadorPuntos){
-        System.out.println(" ".repeat(10)+"Puntuacion: "+contadorPuntos[0]);
+    static void skins(int [] skinActual, int [][] terreno, int [] posPersonaje, int [] posFantasma, int [] contadorPuntos){
+        System.out.println("Puntuacion: ["+contadorPuntos[0]+"]");
+        if (skinActual[0] == 1){
+            mostrarTerreno1(terreno, posPersonaje, posFantasma);
+        }
+    }
+
+    static void mostrarTerreno1(int [][] terreno, int [] posPersonaje, int [] posFantasma){
         for (int laFila = 0; laFila < terreno.length; laFila++) {
             for (int laColumna = 0; laColumna < terreno[laFila].length; laColumna++) {
                 if (laFila == posPersonaje[0] && laColumna == posPersonaje[1]) {
@@ -46,13 +55,16 @@ public class Pacman {
                     else if (terreno[laFila][laColumna] == 2){
                         System.out.print(" ");
                     }
+                    else if (terreno[laFila][laColumna] == 3){
+                        System.out.print("X");
+                    }
                 }
             }
             System.out.println();
         }
     }
 
-    static boolean moverse(int [] posPersonaje, int [] posFantasma){
+    static boolean moverse(int [] posPersonaje, int [] posFantasma, int [] skinActual){
         Scanner entrada = new Scanner(System.in);
         char inputUsuario;
         inputUsuario = entrada.nextLine().charAt(0);
@@ -69,8 +81,15 @@ public class Pacman {
                 case 'd', 'D', '6':
                     posPersonaje[1] = posPersonaje[1] + 1;
                     break;
+                case '0':
+                    skinActual[0] = 0;
+                case '1':
+                skinActual[0] = 1;
+                case '3':
+                skinActual[0] = 3;
                 case 'f', 'F':
                     return true;
+
             }
         return false;
     }
@@ -80,5 +99,27 @@ public class Pacman {
             terreno[posPersonaje[0]][posPersonaje[1]] = 2;
             contadorPuntos[0]+=3;
         }
+    }
+
+    static boolean bolaInvencibilidad(int [][] terreno, int [] posPersonaje, int [] contadorPuntos){
+        if (terreno[posPersonaje[0]][posPersonaje[1]] == 3){
+            terreno[posPersonaje[0]][posPersonaje[1]] = 2;
+            contadorPuntos[0]+=6;
+            return true;
+        }
+        return false;
+    }
+
+    static boolean interaccionFantasma(int [] posFantasma, int [] posPersonaje, int [] contadorPuntos, boolean invencibilidad){
+        if (posFantasma[0] == posPersonaje[0] && posFantasma[1] == posPersonaje[1]){
+            if (invencibilidad){
+                contadorPuntos[0]+=20;
+                posFantasma [0] = 5; 
+                posFantasma [1] = 10;
+                return false;
+            }
+            return true;
+        }
+        return false;
     }
 }
