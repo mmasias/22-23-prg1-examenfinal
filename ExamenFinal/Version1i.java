e4a2b3e23f37f3c3197afb718ab295239ef1d563
Adrián García Arranz <114479608+aadrigaar@users.noreply.github.com>
2023-01-20 09:54:42 +0100

    Add files via upload

--- ExamenFinal/Version1i.java
+++ ExamenFinal/Version1i.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class Pacman {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
 
         Scanner entrada = new Scanner(System.in);
@@ -76,9 +79,81 @@ public class Pacman {
                  }   
             posicionObjetos[i][0] = fila;
             posicionObjetos[i][1] = columna;
-}
+            }
+for (int laFila = 0; laFila < unaMatriz.length; laFila++) {
+    for (int laColumna = 0; laColumna < unaMatriz[laFila].length; laColumna++) {
+        if (laFila == posicionPersonaje[0] && laColumna == posicionPersonaje[1]) {
+            System.out.print("P");
+        } else if (laFila == posicionFantasma[0] && laColumna == posicionFantasma[1]) {
+            System.out.print("F");
+        } else {
+            boolean esObjetivo = false;
+            for (int i = 0; i < 4; i++) {
+                if (laFila == posicionObjetos[i][0] && laColumna == posicionObjetos[i][1]) {
+                    System.out.print("O");
+                    esObjetivo = true;
+                    break;
+                }
+            }
+            if (!esObjetivo) {
+                if (unaMatriz[laFila][laColumna] == 0) {
+                    System.out.print(" ");
+                } else if (unaMatriz[laFila][laColumna] == 1) {
+                    System.out.print("#");
+                }
+                }
+                }
+                }
+                System.out.println();
+                }switch (inputUsuario) {
+                    case 's', 'S', '8':
+                    posicionPersonaje[0] = posicionPersonaje[0] + 1;
+                    for (int i = 0; i < 4; i++) {
+                    if (posicionPersonaje[0] == posicionObjetos[i][0] && posicionPersonaje[1] == posicionObjetos[i][1]) {
+                    posicionObjetos[i][0] = -1;
+                    posicionObjetos[i][1] = -1;
+                    break;
+                    }                    
+                } 
+                        
+                    break;
+                    case 'w', 'W', '2':
+                    posicionPersonaje[0] = posicionPersonaje[0] - 1;
+                    for (int i = 0; i < 4; i++) {
+                    if (posicionPersonaje[0] == posicionObjetos[i][0] && posicionPersonaje[1] == posicionObjetos[i][1]) {
+                    posicionObjetos[i][0] = -1;
+                    posicionObjetos[i][1] = -1;
+                    break;
+                    }
+                    }
+                    break;
+                    case 'a', 'A', '4':
+                    posicionPersonaje[1] = posicionPersonaje[1] - 1;
+                    for (int i = 0; i < 4; i++) {
+                    if (posicionPersonaje[0] == posicionObjetos[i][0] && posicionPersonaje[1] == posicionObjetos[i][1]) {
+                    posicionObjetos[i][0] = -1;
+                    posicionObjetos[i][1] = -1;
+                    break;
+                    }
+                    }
+                    break;
+                    case 'd', 'D', '6':
+                    posicionPersonaje[1] = posicionPersonaje[1] + 1;
+                    for (int i = 0; i < 4; i++) {
+                    if (posicionPersonaje[0] == posicionObjetos[i][0] && posicionPersonaje[1] == posicionObjetos[i][1]) {
+                    posicionObjetos[i][0] = -1;
+                    posicionObjetos[i][1] = -1;
+                    break;
+                    }
+                    }
+                    break;
+                    case 'q', 'Q':
+                    terminar = true;
+                    break;
+                    }
+
     }
-}
+}     
 
 
 
