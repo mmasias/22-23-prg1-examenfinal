480dd4e16e08fa19e2749abaa1e4e7f36d9d11c3
Santiago Gómez de la Torre <102590833+SantiGT1503@users.noreply.github.com>
2023-01-20 09:56:57 +0100

    Update PacmanV1Santiago.java

--- entregas/gomezSantiago/PacmanV1Santiago.java
+++ entregas/gomezSantiago/PacmanV1Santiago.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PacmanBosquejo {
+public class PacmanV1Santiago {
     public static void main(String[] args) {
 
         Scanner entrada = new Scanner(System.in);
