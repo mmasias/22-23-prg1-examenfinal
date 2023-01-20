96db9a52d5ec13623450f03944c09b483141acc6
Diegoza04 <114479536+Diegoza04@users.noreply.github.com>
2023-01-20 03:32:28 -0600

    Update PacmanVersion2.java

--- PacmanVersion2.java
+++ PacmanVersion2.java
@@ -1,58 +1,55 @@
-import java.util.Scanner;
-public class examenfinal {
+public import java.util.Scanner;
+
+public class Pacman{
     public static void main(String[] args) {
 
         Scanner entrada = new Scanner(System.in);
         char inputUsuario;
         boolean terminar = false;
+        int puntos = 0;
+        int invencibilidad = 0;
 
         int[][] unaMatriz = {
                 { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
                 { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
-                { 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1 },
+                { 1, 2, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 2, 1 },
                 { 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1 },
                 { 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1 },
                 { 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0 },
                 { 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1 },
                 { 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1 },
                 { 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1 },
-                { 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1 },
+                { 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1 },
                 { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
                 { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
         };
 
         int[] posicionPersonaje = { 7, 10 };
         int[] posicionFantasma = { 5, 10 };
-        int contadorpastilla=0;
+
         do {
-            System.out.println("Puntos:"+contadorpastilla);
+
+            System.out.println("Puntos: ["+puntos+"] / Invencibilidad: ["+invencibilidad+"]");
             for (int laFila = 0; laFila < unaMatriz.length; laFila++) {
                 for (int laColumna = 0; laColumna < unaMatriz[laFila].length; laColumna++) {
-                    if (posicionPersonaje[0] == laFila && posicionPersonaje[1] == laColumna
-                    && unaMatriz[laFila][laColumna] == 0) {
-                unaMatriz[laFila][laColumna] = 2;
-                
-                    }
-
                     if (laFila == posicionPersonaje[0] && laColumna == posicionPersonaje[1]) {
                         System.out.print("P");
                     } else if (laFila == posicionFantasma[0] && laColumna == posicionFantasma[1]) {
                         System.out.print("F");
-                    
-                    }
-                    else {
+                    } else {
                         if (unaMatriz[laFila][laColumna] == 0) {
                             System.out.print(".");
                         } else if (unaMatriz[laFila][laColumna] == 1) {
                             System.out.print("#");
-                        }else if (unaMatriz[laFila][laColumna]== 2){
+                        } else if (unaMatriz[laFila][laColumna] ==2) {
+                            System.out.print("*");
+                        } else {
                             System.out.print(" ");
-                            contadorpastilla=contadorpastilla+3;
                         }
                     }
                 }
                 System.out.println();
-            } 
+            }
 
             inputUsuario = entrada.nextLine().charAt(0);
             switch (inputUsuario) {
@@ -71,15 +68,35 @@ public class examenfinal {
                 case 'f', 'F':
                     terminar = true;
             }
+
+            switch (posicionPersonaje[0]){
+                case -1:
+                posicionPersonaje[0] = unaMatriz.length;
+                case 22:
+                posicionPersonaje[0] = 0;
+
+            }
+
+            if (invencibilidad!=0){
+                invencibilidad = invencibilidad - 1;
+            }
+
+
             for (int laFila = 0; laFila < unaMatriz.length; laFila++) {
                 for (int laColumna = 0; laColumna < unaMatriz[laFila].length; laColumna++) {
                     if (laFila == posicionPersonaje[0] && laColumna == posicionPersonaje[1] && unaMatriz[laFila][laColumna] == 0) {
                         unaMatriz [laFila][laColumna] = -1;
-                        contadorpastilla = contadorpastilla + 3;
+                        puntos = puntos + 3;
                     }
-       } } while (!terminar);
-    }
-
+                    if (laFila == posicionPersonaje[0] && laColumna == posicionPersonaje[1] && unaMatriz[laFila][laColumna] == 2) {
+                        unaMatriz [laFila][laColumna] = -1;
+                        puntos = puntos + 6;
+                        invencibilidad = invencibilidad + 15;
+                    }
+                }
+            }
+        } while (!terminar);
     }
 }
+
 
