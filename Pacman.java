c6563a67ce70dce16a0d26df324b4d83be90b7ec
OmarBarrios <momarbarrios863@gmail.com>
2023-01-20 10:37:51 +0100

    [v5] implementacion de skins para el juego

--- Pacman.java
+++ Pacman.java
@@ -24,13 +24,21 @@ public class Pacman {
         int laFila = 12;
         int laColumna = 22;
         int puntos = 0;
+        int numeroDeMapa = 1;
 
         int[] posicionPersonaje = { 7, 10 };
         int[] posicionFantasma = { 5, 10 };
 
         do {
             System.out.println("\nPuntos [" + puntos + "]");
-            impresionDelMapa(unaMatriz, posicionPersonaje, posicionFantasma, laFila, laColumna);
+            if (numeroDeMapa == 1) {
+                impresionDelMapa1(unaMatriz, posicionPersonaje, posicionFantasma, laFila, laColumna);
+            } else if (numeroDeMapa == 2) {
+                impresionDelMapa2(unaMatriz, posicionPersonaje, posicionFantasma, laFila, laColumna);
+            } else if (numeroDeMapa == 3) {
+                impresionDelMapa3(unaMatriz, posicionPersonaje, posicionFantasma, laFila, laColumna);
+            }
+
             inputUsuario = entrada.nextLine().charAt(0);
             switch (inputUsuario) {
                 case 's', 'S', '8':
@@ -73,6 +81,13 @@ public class Pacman {
                     }
                     posicionPersonaje[1] = posicionPersonaje[1] + 1;
                     break;
+                case 'v', 'V':
+                    if (numeroDeMapa == 3) {
+                        numeroDeMapa = 1;
+                    } else {
+                        numeroDeMapa++;
+                    }
+                    break;
                 case 'f', 'F':
                     terminar = true;
             }
@@ -80,43 +95,7 @@ public class Pacman {
         } while (!terminar);
     }
 
-    static void movimientosUsuario(char inputUsuario, int[] posicionPersonaje, boolean terminar, int[][] unaMatriz,
-            int puntos) {
-        switch (inputUsuario) {
-            case 's', 'S', '8':
-                if (unaMatriz[posicionPersonaje[0]][posicionPersonaje[1]] == 0) {
-                    unaMatriz[posicionPersonaje[0]][posicionPersonaje[1]] = 2;
-                    puntos = puntos + 3;
-                }
-                posicionPersonaje[0] = posicionPersonaje[0] + 1;
-                break;
-            case 'w', 'W', '2':
-                if (unaMatriz[posicionPersonaje[0]][posicionPersonaje[1]] == 0) {
-                    unaMatriz[posicionPersonaje[0]][posicionPersonaje[1]] = 2;
-                    puntos = puntos + 3;
-                }
-                posicionPersonaje[0] = posicionPersonaje[0] - 1;
-                break;
-            case 'a', 'A', '4':
-                if (unaMatriz[posicionPersonaje[0]][posicionPersonaje[1]] == 0) {
-                    unaMatriz[posicionPersonaje[0]][posicionPersonaje[1]] = 2;
-                    puntos = puntos + 3;
-                }
-                posicionPersonaje[1] = posicionPersonaje[1] - 1;
-                break;
-            case 'd', 'D', '6':
-                if (unaMatriz[posicionPersonaje[0]][posicionPersonaje[1]] == 0) {
-                    unaMatriz[posicionPersonaje[0]][posicionPersonaje[1]] = 2;
-                    puntos = puntos + 3;
-                }
-                posicionPersonaje[1] = posicionPersonaje[1] + 1;
-                break;
-            case 'f', 'F':
-                terminar = true;
-        }
-    }
-
-    static void impresionDelMapa(int[][] unaMatriz, int[] posicionPersonaje, int[] posicionFantasma, int laFila,
+    static void impresionDelMapa1(int[][] unaMatriz, int[] posicionPersonaje, int[] posicionFantasma, int laFila,
             int laColumna) {
         for (laFila = 0; laFila < unaMatriz.length; laFila++) {
             for (laColumna = 0; laColumna < unaMatriz[laFila].length; laColumna++) {
@@ -131,6 +110,50 @@ public class Pacman {
                         System.out.print("#");
                     } else if (unaMatriz[laFila][laColumna] == 2) {
                         System.out.print(" ");
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    static void impresionDelMapa2(int[][] unaMatriz, int[] posicionPersonaje, int[] posicionFantasma, int laFila,
+            int laColumna) {
+        for (laFila = 0; laFila < unaMatriz.length; laFila++) {
+            for (laColumna = 0; laColumna < unaMatriz[laFila].length; laColumna++) {
+                if (laFila == posicionPersonaje[0] && laColumna == posicionPersonaje[1]) {
+                    System.out.print("PP");
+                } else if (laFila == posicionFantasma[0] && laColumna == posicionFantasma[1]) {
+                    System.out.print("FF");
+                } else {
+                    if (unaMatriz[laFila][laColumna] == 0) {
+                        System.out.print("()");
+                    } else if (unaMatriz[laFila][laColumna] == 1) {
+                        System.out.print("##");
+                    } else if (unaMatriz[laFila][laColumna] == 2) {
+                        System.out.print("  ");
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    static void impresionDelMapa3(int[][] unaMatriz, int[] posicionPersonaje, int[] posicionFantasma, int laFila,
+            int laColumna) {
+        for (laFila = 0; laFila < unaMatriz.length; laFila++) {
+            for (laColumna = 0; laColumna < unaMatriz[laFila].length; laColumna++) {
+                if (laFila == posicionPersonaje[0] && laColumna == posicionPersonaje[1]) {
+                    System.out.print("PPP");
+                } else if (laFila == posicionFantasma[0] && laColumna == posicionFantasma[1]) {
+                    System.out.print("FFF");
+                } else {
+                    if (unaMatriz[laFila][laColumna] == 0) {
+                        System.out.print(" O ");
+                    } else if (unaMatriz[laFila][laColumna] == 1) {
+                        System.out.print("###");
+                    } else if (unaMatriz[laFila][laColumna] == 2) {
+                        System.out.print("   ");
                     }
                 }
             }
