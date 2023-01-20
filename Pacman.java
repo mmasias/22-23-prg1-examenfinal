976f10ef655d84a23d2a4f63c356cb9964f524f9
rubensetien <114570479+rubensetien@users.noreply.github.com>
2023-01-20 09:20:47 +0100

    Update Pacman.java

--- Pacman.java
+++ Pacman.java
@@ -1,13 +1,28 @@
 import java.util.Scanner;
 
 public class Pacman {
+
+    static final int FILA = 0;
+    static final int COLUMNA = 1;
+
+    static final int ARRIBA = 1;
+    static final int ABAJO = 2;
+    static final int IZQUIERDA = 3;
+    static final int DERECHA = 4;
+
+    static final int[][] MOVIMIENTOS = {
+            {0, 0},
+            {-1, 0},
+            {1, 0},
+            {0, -1},
+            {0, 1}
+    };
+
+
+    static int puntos=0;
     public static void main(String[] args) {
 
-        Scanner entrada = new Scanner(System.in);
-        char inputUsuario;
-        boolean terminar = false;
-
-        int[][] unaMatriz = {
+        int[][] elMapa = {
                 { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
                 { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
                 { 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1 },
@@ -22,44 +37,158 @@ public class Pacman {
                 { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
         };
 
-        int[] posicionPersonaje = { 7, 10 };
-        int[] posicionFantasma = { 5, 10 };
+        int[] elPersonaje = {7, 10};
+        int[][] fantasma = {{5, 10}};
 
         do {
-            for (int laFila = 0; laFila < unaMatriz.length; laFila++) {
-                for (int laColumna = 0; laColumna < unaMatriz[laFila].length; laColumna++) {
-                    if (laFila == posicionPersonaje[0] && laColumna == posicionPersonaje[1]) {
-                        System.out.print("P");
-                    } else if (laFila == posicionFantasma[0] && laColumna == posicionFantasma[1]) {
-                        System.out.print("F");
-                    } else {
-                        if (unaMatriz[laFila][laColumna] == 0) {
-                            System.out.print(" ");
-                        } else if (unaMatriz[laFila][laColumna] == 1) {
-                            System.out.print("#");
-                        }
-                    }
+            imprimeMundo(elMapa, elPersonaje, fantasma);
+
+            definePosicion(elPersonaje, elMapa);
+        } while ((comprobar(elPersonaje, fantasma) != 1)&&contador(elMapa)!=0);
+    }
+
+
+    private static void definePosicion(int[] elPersonaje, int[][] elMapa) {
+
+        switch (capturaMovimiento()) {
+            case ARRIBA:
+                mueve(elPersonaje, ARRIBA, elMapa);
+                break;
+            case ABAJO:
+                mueve(elPersonaje, ABAJO, elMapa);
+                break;
+            case IZQUIERDA:
+                mueve(elPersonaje, IZQUIERDA, elMapa);
+                break;
+            case DERECHA:
+                mueve(elPersonaje, DERECHA, elMapa);
+                break;
+        }
+    }
+
+
+
+
+
+
+
+    private static int capturaMovimiento() {
+
+        switch (preguntaChar()) {
+            case 's', 'S', '8':
+                return ABAJO;
+            case 'w', 'W', '2':
+                return ARRIBA;
+            case 'a', 'A', '4':
+                return IZQUIERDA;
+            case 'd', 'D', '6':
+                return DERECHA;
+        }
+        return 0;
+    }
+
+    static void imprimeMundo(int[][] unMapa, int[] unPersonaje, int[][] fantasma) {
+
+        for (int fila = 0; fila < unMapa.length; fila++) {
+            for (int columna = 0; columna < unMapa[fila].length; columna++) {
+                if (fila == unPersonaje[FILA] && columna == unPersonaje[COLUMNA]) {
+                    imprimePersonaje();
+
+
+                } else if (existeFantasma(fila, columna, fantasma)) {
+                    imprimeFantasma();
+                } else {
+                    imprimeTerreno(unMapa[fila][columna]);
                 }
-                System.out.println();
+
+
+            }
+            System.out.println();
+
+        }System.out.println("Puntuacion: "+puntos);
+    }
+
+    static void imprimeTerreno(int unTile) {
+
+        String[] terreno = {".", "#", " "};
+        System.out.print(terreno[unTile]);
+    }
+
+    static void imprimePersonaje() {
+
+        System.out.print("P");
+    }
+
+    static void imprimeFantasma() {
+        System.out.print("F");
+    }
+
+    static void mueve(int[] elPersonaje, int movimiento, int[][] elMapa) {
+
+        int nuevaFila = elPersonaje[FILA] + MOVIMIENTOS[movimiento][FILA];
+        int nuevaColumna = elPersonaje[COLUMNA] + MOVIMIENTOS[movimiento][COLUMNA];
+
+            if (elMapa[elPersonaje[FILA]][elPersonaje[COLUMNA]]==0){
+                puntos+=3;
+                elMapa[elPersonaje[FILA]][elPersonaje[COLUMNA]] = 2;
             }
 
-            inputUsuario = entrada.nextLine().charAt(0);
-            switch (inputUsuario) {
-                case 's', 'S', '8':
-                    posicionPersonaje[0] = posicionPersonaje[0] + 1;
-                    break;
-                case 'w', 'W', '2':
-                    posicionPersonaje[0] = posicionPersonaje[0] - 1;
-                    break;
-                case 'a', 'A', '4':
-                    posicionPersonaje[1] = posicionPersonaje[1] - 1;
-                    break;
-                case 'd', 'D', '6':
-                    posicionPersonaje[1] = posicionPersonaje[1] + 1;
-                    break;
-                case 'f', 'F':
-                    terminar = true;
+            elPersonaje[FILA] = nuevaFila;
+            elPersonaje[COLUMNA] = nuevaColumna;
+        }
+
+
+
+
+
+
+
+
+    static char preguntaChar() {
+
+        Scanner entrada = new Scanner(System.in);
+        return entrada.next().charAt(0);
+    }
+
+
+
+    static int comprobar(int elPersonaje[], int fantasma[][]) {
+        if (existeFantasma(elPersonaje[FILA], elPersonaje[COLUMNA], fantasma)) {
+            System.out.print("GAME OVER");
+            return 1;
+        }
+        return 0;
+    }
+    static int contador( int elMapa [][]) {
+        int puntitos = 0;
+        for (int i = 0; i < elMapa.length; i++) {
+            for (int j = 0; j < elMapa.length; j++) {
+                if (elMapa[i][j] == 1) {
+                    puntitos++;
+                }
+
             }
-        } while (!terminar);
+
+        }return puntitos;
+    }
+
+
+
+
+
+
+    static boolean existeFantasma(int f, int c, int[][] fantasma) {
+        for (int i = 0; i < fantasma.length; i++) {
+
+
+            if (f == fantasma[i][FILA] && c == fantasma[i][COLUMNA]) {
+
+                return true;
+
+            }
+
+        }
+        return false;
+
     }
 }
