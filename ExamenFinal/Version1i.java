ad13ef78d8f630caf8587096a3e8d03f712b8db2
Adrián García Arranz <114479608+aadrigaar@users.noreply.github.com>
2023-01-20 09:25:41 +0100

    Add files via upload

---
+++ ExamenFinal/Version1i.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+
+public class Pacman {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        char inputUsuario;
+        boolean terminar = false;
+
+        int[][] unaMatriz = {
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                { 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1 },
+                { 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1 },
+                { 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1 },
+                { 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0 },
+                { 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1 },
+                { 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1 },
+                { 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1 },
+                { 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1 },
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
+        };
+
+        int[] posicionPersonaje = { 7, 10 };
+        int[] posicionFantasma = { 5, 10 };
+
+
+        do {
+            for (int laFila = 0; laFila < unaMatriz.length; laFila++) {
+                for (int laColumna = 0; laColumna < unaMatriz[laFila].length; laColumna++) {
+                    if (laFila == posicionPersonaje[0] && laColumna == posicionPersonaje[1]) {
+                        System.out.print("P");
+                    } else if (laFila == posicionFantasma[0] && laColumna == posicionFantasma[1]) {
+                        System.out.print("F");
+                    } else {
+                        if (unaMatriz[laFila][laColumna] == 0) {
+                            System.out.print(" ");
+                        } else if (unaMatriz[laFila][laColumna] == 1) {
+                            System.out.print("#");
+                        }
+                    }
+                }
+                System.out.println();
+            }
+
+            inputUsuario = entrada.nextLine().charAt(0);
+            switch (inputUsuario) {
+                case 's', 'S', '8':
+                    posicionPersonaje[0] = posicionPersonaje[0] + 1;
+                    break;
+                case 'w', 'W', '2':
+                    posicionPersonaje[0] = posicionPersonaje[0] - 1;
+                    break;
+                case 'a', 'A', '4':
+                    posicionPersonaje[1] = posicionPersonaje[1] - 1;
+                    break;
+                case 'd', 'D', '6':
+                    posicionPersonaje[1] = posicionPersonaje[1] + 1;
+                    break;
+                case 'f', 'F':
+                    terminar = true;
+            }
+        } while (!terminar);
+
+        Random r = new Random();
+
+            int[] posicionObjetos = new int[4][2];
+
+            for (int i = 0; i < 4; i++) {
+                 int fila = r.nextInt(unaMatriz.length);
+                int columna = r.nextInt(unaMatriz[0].length);
+                while (unaMatriz[fila][columna] == 1 || (fila == posicionPersonaje[0] && columna == posicionPersonaje[1]) || (fila == posicionFantasma[0] && columna == posicionFantasma[1])) {
+                 fila = r.nextInt(unaMatriz.length);
+                 columna = r.nextInt(unaMatriz[0].length);
+                 }   
+            posicionObjetos[i][0] = fila;
+            posicionObjetos[i][1] = columna;
+}
+    }
+}
+
+
+
+
