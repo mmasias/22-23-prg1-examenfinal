78005b91ff077be74d60e600d99b23189cf448cb
Pablo <pablo.arce1@alumnos.uneatlantico.es>
2023-01-20 09:22:10 +0100

    Primera media hora

--- Pacman.java
+++ Pacman.java
@@ -1,13 +1,10 @@
-import java.util.Scanner;
-
+import java.util.*;
 public class Pacman {
     public static void main(String[] args) {
 
-        Scanner entrada = new Scanner(System.in);
-        char inputUsuario;
         boolean terminar = false;
 
-        int[][] unaMatriz = {
+        int[][] terreno = {
                 { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
                 { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
                 { 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1 },
@@ -22,44 +19,66 @@ public class Pacman {
                 { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
         };
 
-        int[] posicionPersonaje = { 7, 10 };
-        int[] posicionFantasma = { 5, 10 };
+        int[] posPersonaje = { 7, 10 };
+        int[] posFantasma = { 5, 10 };
+        int[] contadorPuntos = {0};
+        while(terminar == false){
+            mostrarTerreno(terreno, posPersonaje, posFantasma, contadorPuntos);
+            terminar = moverse(posPersonaje, posFantasma);
+            comerBolitas(terreno, posPersonaje, contadorPuntos);
+        }
+    }
 
-        do {
-            for (int laFila = 0; laFila < unaMatriz.length; laFila++) {
-                for (int laColumna = 0; laColumna < unaMatriz[laFila].length; laColumna++) {
-                    if (laFila == posicionPersonaje[0] && laColumna == posicionPersonaje[1]) {
-                        System.out.print("P");
-                    } else if (laFila == posicionFantasma[0] && laColumna == posicionFantasma[1]) {
-                        System.out.print("F");
-                    } else {
-                        if (unaMatriz[laFila][laColumna] == 0) {
-                            System.out.print(" ");
-                        } else if (unaMatriz[laFila][laColumna] == 1) {
-                            System.out.print("#");
-                        }
+    static void mostrarTerreno(int [][] terreno, int [] posPersonaje, int [] posFantasma, int [] contadorPuntos){
+        System.out.println(" ".repeat(10)+"Puntuacion: "+contadorPuntos[0]);
+        for (int laFila = 0; laFila < terreno.length; laFila++) {
+            for (int laColumna = 0; laColumna < terreno[laFila].length; laColumna++) {
+                if (laFila == posPersonaje[0] && laColumna == posPersonaje[1]) {
+                    System.out.print("P");
+                } else if (laFila == posFantasma[0] && laColumna == posFantasma[1]) {
+                    System.out.print("F");
+                } else {
+                    if (terreno[laFila][laColumna] == 0) {
+                        System.out.print("·");
+                    } else if (terreno[laFila][laColumna] == 1) {
+                        System.out.print("#");
+                    }
+                    else if (terreno[laFila][laColumna] == 2){
+                        System.out.print(" ");
                     }
                 }
-                System.out.println();
             }
+            System.out.println();
+        }
+    }
 
-            inputUsuario = entrada.nextLine().charAt(0);
+    static boolean moverse(int [] posPersonaje, int [] posFantasma){
+        Scanner entrada = new Scanner(System.in);
+        char inputUsuario;
+        inputUsuario = entrada.nextLine().charAt(0);
             switch (inputUsuario) {
                 case 's', 'S', '8':
-                    posicionPersonaje[0] = posicionPersonaje[0] + 1;
+                    posPersonaje[0] = posPersonaje[0] + 1;
                     break;
                 case 'w', 'W', '2':
-                    posicionPersonaje[0] = posicionPersonaje[0] - 1;
+                    posPersonaje[0] = posPersonaje[0] - 1;
                     break;
                 case 'a', 'A', '4':
-                    posicionPersonaje[1] = posicionPersonaje[1] - 1;
+                    posPersonaje[1] = posPersonaje[1] - 1;
                     break;
                 case 'd', 'D', '6':
-                    posicionPersonaje[1] = posicionPersonaje[1] + 1;
+                    posPersonaje[1] = posPersonaje[1] + 1;
                     break;
                 case 'f', 'F':
-                    terminar = true;
+                    return true;
             }
-        } while (!terminar);
+        return false;
+    }
+    
+    static void comerBolitas(int [][] terreno, int [] posPersonaje, int [] contadorPuntos){
+        if (terreno[posPersonaje[0]][posPersonaje[1]] == 0){
+            terreno[posPersonaje[0]][posPersonaje[1]] = 2;
+            contadorPuntos[0]+=3;
+        }
     }
 }
